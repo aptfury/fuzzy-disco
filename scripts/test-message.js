06d912537607910e7719de990d89faa88059730d
aptfury <blake.lemarr@outlook.com>
2024-11-20 20:58:54 -0500

    author to user

--- scripts/test-message.js
+++ scripts/test-message.js
@@ -1,3 +1,3 @@
 Hooks.once("init", async function() {
-    await ChatMessage.create({content: "hello", author: "STrVxwVEHXp9QCoA"});
+    await ChatMessage.create({content: "hello", user: "STrVxwVEHXp9QCoA"});
 });
