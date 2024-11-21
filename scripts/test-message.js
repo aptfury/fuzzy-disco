7e338daa198a5cdc079118edd20f6f3b332e3886
aptfury <blake.lemarr@outlook.com>
2024-11-20 19:29:12 -0500

    message construction hopefully

--- scripts/test-message.js
+++ scripts/test-message.js
@@ -1,5 +1,11 @@
 export function startMessage() {
+    const data = [
+        {
+            flavor: "A Test Message",
+            content: "This is a message to chat for once the software is ready."
+        }
+    ];
     Hooks.on("ready", function() {
-        Messages.create("This is a message to chat for once the software is ready.");
+        ChatMessage.create(data);
     });
 }
